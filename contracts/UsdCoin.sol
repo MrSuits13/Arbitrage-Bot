d0f09993caa085d51924a502e4eba212121a5b45
MrSuits13 <107210589+MrSuits13@users.noreply.github.com>
2023-05-03 07:06:54 +0200

    Arbitrage Bot First Commit

---
+++ contracts/UsdCoin.sol
@@ -0,0 +1,14 @@
+pragma solidity =0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDCoin is ERC20, Ownable {
+    constructor() ERC20("UsdCoin", "USDC") {
+
+    }
+    function mint (address to, uint256 amount) public onlyOwner {
+        _mint(to, amount)
+    }
+
+}
